6a2914950f9f6b4a6478205c32fc542181f52565
Your Name <tsimbalenko.vv@gmail.com>
2024-06-04 08:50:31 +0300

    14

---
+++ src/components/Golovna/Golovna.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+
+
+function Golovna() {
+  return (
+    <div>
+    Golovna
+    </div>
+  )
+}
+export {Golovna}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "./logo/icon.jpg";
+import "./header.css"
+
+
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">
+        <Link to="/"><img src={logo} alt="logo"/></Link>
+      </div>
+      <div className="menu">
+        <ul>
+          <li>
+            <Link to="/about">ABOUT AS</Link>{" "}
+          </li>
+          <li>
+            <Link to="/contact">CONTACT</Link>{" "}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export { Header };
